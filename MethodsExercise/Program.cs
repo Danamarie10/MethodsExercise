1d3ef6e074f8c826b55fb512bcb383ea6facca05
Danamarie10 <dana.patrick10@outlook.com>
2023-05-12 04:01:05 -0400

    finished main method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+            //Your name: Dana
+            //Favorite Color: Green
+            //Favorite Animal:Raccoon 
+            //Favorite Band: All time Low 
+            
+            Console.WriteLine("What is your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName} What is your favorite color?");
+            var Color = Console.ReadLine();
+
+            Console.WriteLine($"{Color} is a great color, what, what is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine($"Awesome! Now, what is your favorite band?");
+            var band = Console.ReadLine();
         }
     }
 }
